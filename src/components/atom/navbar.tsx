f536d35dbc066edb9e862c2fe0bceaf42c8f175c
frebarcelos <96388323+frebarcelos@users.noreply.github.com>
2024-06-20 11:13:33 -0300

    fix: troca da forma do deploy

--- src/components/atom/navbar.tsx
+++ src/components/atom/navbar.tsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavBar() {
+const NavBar: React.FC = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" bg="primary" data-bs-theme="dark">
       <Container>
